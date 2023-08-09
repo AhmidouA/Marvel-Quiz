c0b02b94224d5eacf1d256ebfea25093ca52880a
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-09 11:13:47 +0200

    Landing home page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,13 @@ import '../../App.css';
 
 // Component
 import Header from '../Header';
+import Landing from '../Landing';
 
 function App() {
   return (
     <div>
       <Header />
+      <Landing />
     </div>
   );
 }
