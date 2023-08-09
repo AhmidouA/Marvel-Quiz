0fa6008b5f4e8e1bde04436db902955a171bad24
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-09 11:07:06 +0200

    Header home page

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+
+// Component
+import Header from '../Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
