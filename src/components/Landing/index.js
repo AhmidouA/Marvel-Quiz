d01a97219bc610230fe53d94a454744ea03c641d
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-09 13:47:12 +0200

    onMouseOuver and onMouseOut wolverine

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -6,13 +6,13 @@ const Landing = () => {
 
     // useState = State des class
     // state pour les btn 
-    const [btn, setBtn] = useState(false)
+    const [btn, setBtn] = useState(false);
     
 
 
     // useRef pour obtenir le current afin de manipuler le DOM (=getElementbyId ou le querySelector)
     // Maintentn refWolverine = current.main.welcomePage
-    const refWolverine = useRef(null)
+    const refWolverine = useRef(null);
     // console.log("refWolverine Ref", refWolverine)
 
 
@@ -28,10 +28,30 @@ const Landing = () => {
 
             // une fois les griffe disparu on fait apparaitre les button
             setBtn(true)
-
         }, 1000); // 1 seconde 
 
-    }, []) // on met ou laisse le array vide car on veut qu'elle s'excute seulement au montage et non aprés
+    }, []); // on met ou laisse le array vide car on veut qu'elle s'excute seulement au montage et non aprés
+
+
+    // methode Over pour les btn 
+    // on ajoute les griffe gauche seulement quand on passe sur le btn gauche
+    const setLeftImg = () => {
+        refWolverine.current.classList.add("leftImg")
+    };
+
+    // on ajoute les griffe gauche seulement quand on passe sur le btn droite
+    const setRightImg = () => {
+        refWolverine.current.classList.add("rightImg")
+    };
+
+    // Rentialise et enleve la class quand on enleve la souris 
+    const clearImg = () => {
+        // on vérifie si elle ne contient pas la class (grace a contains)
+        if(!refWolverine.current.classList.contains("leftImg")) {
+            return refWolverine.current.classList.add("leftImg")
+        } 
+        return refWolverine.current.classList.remove("rightImg") 
+    };
 
 
     // methode pour les button
@@ -39,11 +59,11 @@ const Landing = () => {
         if (btn === true) {
             return (
                 <React.Fragment> {/* React Fragments: permet de regrouper plusieurs éléments enfants sans ajouter d'élément DOM supplémentair */}         
-                    <div className='leftBox'>
+                    <div onMouseOver={setLeftImg} onMouseOut={clearImg} className='leftBox'>
                         <button className='btn-welcome'>Inscription</button>
                     </div>
 
-                    <div className='rightBox'>
+                    <div onMouseOver={setRightImg} onMouseOut={clearImg} className='rightBox'>
                         <button className='btn-welcome'>Connexion</button>
                     </div>
                 </React.Fragment>
@@ -52,6 +72,9 @@ const Landing = () => {
     
 
 
+
+
+    
   return (
     <main ref={refWolverine} className='welcomePage'>
         {dispalyBtn()}
