1f051661e9400d99105382a1930cfaaf450ab671
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-10 10:43:21 +0200

    Landing page and path with link for button

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,40 @@
+// React
 import React from 'react';
+
+// React Route (BrowserRouter, Route, Routes) 
+// Redirect avec Navigate
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Css
 import '../../App.css';
 
 // Component
 import Header from '../Header';
 import Landing from '../Landing';
 import Footer from '../Footer';
-
+import Welcome from '../Welcome';
+import Login from '../Login';
+import SignUp from '../SignUp';
+import ErrorPage from '../ErrorPage';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Landing />
-      <Footer />
-    </div>
+
+      <Router>
+        <Header />
+        {/* <Routes> a remplacé <Switch> */}
+        <Routes >
+            {/* Les routes pour pouvoir les utiliser */}
+            <Route path='/' element={<Landing />} />
+            <Route path='/welcome' element={<Welcome />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<SignUp />} />
+
+            {/* Error Page:  L'etoile * veut dire All page non trouvé */}
+            <Route path='*' element={<ErrorPage />} />                        
+        </Routes> 
+        <Footer />    
+      </Router>    
   );
 }
 
