ae82bfe2e69a8e0431313174f79e70cdef19634b
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-14 17:55:36 +0200

    useState data and handleChange methode

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SignUp = () => {
+
+  // data du form 
+  const data = {
+    username:'',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+
+
+  // State
+  const [loginData, setloginData] = useState(data)
+
+  // Destructuring pour les values du form 
+  const {username, email, password, confirmPassword} = loginData
+
+  /* methode plus complexe
+  // methode pour récuperer la data
+  const handleChange = (event) => {
+    // avec le spread operator pour récupérer tout l'objet
+    setloginData({...loginData, 
+      // on prend l'id de l'event (data) qu'on souhaite changer et on la change avec la valeur entré
+      [event.target.id]: event.target.value })
+
+  }
+  */
+
+  /*  Ma methode  */
+  const handleChange = (event) => {
+    // event.target fait référence à l'élément (champ de formulaire) qui a provoqué l'événement
+    const { id, value } = event.target;
+    switch (id) {
+      case "username":
+        setloginData({ ...loginData, username: value });
+        break;
+      case "email":
+        setloginData({ ...loginData, email: value });
+        break;
+      case "password":
+        setloginData({ ...loginData, password: value });
+        break;
+      case "confirmPassword":
+        setloginData({ ...loginData, confirmPassword: value });
+        break;
+      default:
+        break;
+    }
+  }
+
+
   return (
     <div className='signUpLoginBox'>
         <div className='slContainer'>
@@ -12,22 +62,22 @@ const SignUp = () => {
                 <h2>Inscription</h2>
                   <form>                 
                       <div className='inputBox'>
-                        <input type='text' id="username" autoComplete='off' required/>
+                        <input onChange={handleChange} value={username} type='text' id="username" autoComplete='off' required/>
                         <label htmlFor='username'>Nom</label>                      
                       </div>
 
                       <div className='inputBox'>
-                        <input type='email' id="email" autoComplete='off' required/>
+                        <input onChange={handleChange} value={email} type='email' id="email" autoComplete='off' required/>
                         <label htmlFor='email'>Email</label>                      
                       </div>
 
                       <div className='inputBox'>
-                        <input type='text' id="password" autoComplete='off' required/>
+                        <input onChange={handleChange} value={password} type='text' id="password" autoComplete='off' required/>
                         <label htmlFor='password'>Mot de passe</label>                      
                       </div>
 
                       <div className='inputBox'>
-                        <input type='password' id="confirmPassword" autoComplete='off' required/>
+                        <input onChange={handleChange} value={confirmPassword} type='password' id="confirmPassword" autoComplete='off' required/>
                         <label htmlFor='confirmPassword'>Confimer le mot de pass</label>                      
                       </div>
                   </form>
