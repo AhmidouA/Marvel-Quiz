759b398fca3ea3bccab7b1724206c59d7ea2ac07
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-15 20:37:45 +0200

    SignUp Done

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,21 +17,30 @@ const firebaseConfig = {
 // Api
 class Firebase {
     constructor() {
-        initializeApp(firebaseConfig) 
+        // Initialise l'application Firebase avec la configuration
+        initializeApp(firebaseConfig)
+
+        // Obtient l'objet d'authentification Firebase
+        /* La fonction getAuth() est utilisée pour obtenir une instance de l'objet d'authentification Firebase. 
+        Cet objet d'authentification est une interface qui fournit des méthodes pour gérer 
+        l'authentification des utilisateurs dans votre application */
         this.auth = getAuth();
               
     }
   
     // SignUp
     signUpUser = (email, password) => 
+    // Crée un nouvel utilisateur avec l'adresse e-mail et le mot de passe fournis
     createUserWithEmailAndPassword(this.auth, email, password)
 
     // Login
     loginUser = (email, password) => 
+    // Connecte un utilisateur avec l'adresse e-mail et le mot de passe fournis
     signInWithEmailAndPassword(this.auth, email, password)
 
     // SignOut
     signOutUser = () => 
+    // Déconnecte l'utilisateur actuellement connecté
     signOut(this.auth)
     
     
