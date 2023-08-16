cc73de8fed1746b9ebedbd82798d181d5486adaa
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-16 13:44:38 +0200

    Login button confirmation Done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,30 +1,60 @@
 // React
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 // Utilisez Link pour un lien sans refreshir
 // Utilisez le hooks useNavigate pour rediriger l'utilisateur
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
   // state pour la connexion
-  const [email, setemail] = useState('');
-  const [password, setpassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const [btn, setbtn] = useState(false) // pour faire apparaitre le button de confirmation
 
 
   // methode pour l'event email et password
   const handleEmail = (event) => {
     // event.target fait référence à l'élément (champ de formulaire) qui a provoqué l'événement
-    setemail(event.target.value);
+    setEmail(event.target.value);
 
   };
   // methode pour l'event password
   const handlePassword = (event) => {
     // event.target fait référence à l'élément (champ de formulaire) qui a provoqué l'événement
-    setemail(event.target.value);
-  }
+    setPassword(event.target.value);
+  };
 
-  
+
+  /* L’utilisation de mutations, abonnements, horloges, messages de journalisation, et autres effets de bord 
+  n’est pas autorisée au sein du corps principal d’une fonction composant (qu’on appelle la phase de rendu de React). 
+  Autrement ça pourrait entraîner des bugs déconcertants et des incohérences dans l’interface utilisateur (UI). 
+  */
+  useEffect(() => {
+    if (password.length > 5 && email !== '') {
+      setbtn(true)    
+
+      // On re initalise le btn pour le disabled
+    } else if (btn === true) {
+      setbtn(false)
+    }
+
+    // chaque fois que la valeur de password ou email change, le code à l'intérieur du useEffect sera exécuté. 
+    // Plus précisément, il vérifie si la longueur du mot de passe est supérieure à 5 caractères et si 
+  }, [password, email]) 
+
+  // Methode pour faire apparaitre le btn selon la condition
+  const btnDisplay = () => {
+    if (btn === false ) {
+      return <button disabled>Connexion</button>
+    }
+    return <button>Connexion</button>
+  }
+   
+
+
+
+
   return (
     <div className='signUpLoginBox'>
         <div className='slContainere'>
@@ -46,6 +76,9 @@ const Login = () => {
                         <input onChange={handlePassword} value={password} type='password' autoComplete='off' required/>
                         <label htmlFor='password'>Mot de passe</label>                      
                       </div>
+
+                      {/* condition pour le btn */}
+                      {btnDisplay()}
                       
                   </form>
                   <div className='linkContainer'>
