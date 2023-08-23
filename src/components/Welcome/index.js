54de914fd2c7c7eea1b70516808e67aea0eb0251
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-23 13:47:18 +0200

    Quiz Component init

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Logout from '../Logout';
+import Quiz from '../Quiz';
 
 const Welcome = () => {
   return (
     <div className='quiz-bg'>
         <div className='container'>
             <Logout />
+            <Quiz /> 
         </div>
     </div>
   )
