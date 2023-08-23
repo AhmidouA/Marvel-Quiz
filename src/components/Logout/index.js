be8f113edee825e24281064332314da392378917
AhmidouA <ahmedakkioui75@gmail.com>
2023-08-23 14:02:38 +0200

    Firebase signOutUser Done

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
+
+//bdd
+import { FirebaseContext } from '../Firebase';
 
 const Logout = () => {
+
+  // useContext pour récuper le provider (les data et methode) de Firebase
+  const firebase = useContext(FirebaseContext) 
+
+  // state pour le btn deconnexion (on le met en false de base)
+  const [checked, setchecked] = useState(false)
+
+  console.log("checked", checked)
+
+  useEffect(() => {
+    if (checked === true) {
+        console.log("deconnexion")
+        // methode SignOut
+        firebase.signOutUser()
+    }
+ 
+  }, [checked])
+  
+
+  const handleChange = (event) => {
+      // console.log("event>>>>", event.target)
+      setchecked(event.target.checked)
+  }
+
   return (
-    <div>
-        Logout
+    <div className="logoutContainer">
+      <label className="switch">
+        <input 
+          onChange={handleChange}
+          type="checkbox" 
+          checked={checked}
+        />
+        <span className="slider round"></span>
+      </label>
+        
     </div>
   )
 }
